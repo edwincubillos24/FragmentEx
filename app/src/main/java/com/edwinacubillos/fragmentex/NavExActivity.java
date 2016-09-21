fbb45aed6cf632637d718cfd77cc70d53383321d
Edwin <edwinacubillos@gmail.com>
2016-09-21 17:41:51 -0500

    Initial commit

---
+++ app/src/main/java/com/edwinacubillos/fragmentex/NavExActivity.java
@@ -0,0 +1,82 @@
+package com.edwinacubillos.fragmentex;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class NavExActivity extends AppCompatActivity {
+
+    private String[] opciones = new String[] {"Superman", "Batman", "Flash","todos"};
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_ex);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.contenedorPrincipal);
+        listView = (ListView) findViewById(R.id.menuIzq);
+
+        listView.setAdapter(new ArrayAdapter<String>(getSupportActionBar().getThemedContext(),
+                android.R.layout.simple_list_item_1, opciones));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Fragment fragment = null;
+                switch (i){
+                    case(0): fragment = new SupermanFragment(); break;
+                    case(1): fragment = new BatmanFragment(); break;
+                    case(2): fragment = new FlashFragment(); break;
+                    case(3): Intent intent = new Intent(NavExActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+                }
+                if (i != 3) {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.contenedorFrame, fragment).commit();
+
+                }
+               listView.setItemChecked(i,true);
+                drawerLayout.closeDrawer(listView);
+            }
+        });
+
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.abierto, R.string.cerrado);
+
+        drawerLayout.setDrawerListener(drawerToggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(Gravity.LEFT);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
